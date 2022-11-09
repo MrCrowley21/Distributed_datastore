a49896dba2f6eb4a24a883d54da689a650d9b39e
MrCrowley21 <graur.lenka@gmail.com>
2022-11-09 11:29:32 +0200

    Set leader and CRUD methods

--- Components_logic/server_communication.py
+++ Components_logic/server_communication.py
@@ -1,5 +1,10 @@
 import socket
 import logging
+import json
+import random
+import threading
+from time import sleep
+from copy import deepcopy
 
 from config import *
 
@@ -8,17 +13,62 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 class ServerCommunication:
+    def __init__(self):
+        self.app = None
+        self.leader_value = random.randint(0, 100)
+        self.leader_data = {}
+        self.is_partition_leader = False
+        self.partition_leader_exists = False
+
     def send_data(self, data, udp_to_send):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        prepared_data = str.encode(data)
-        sock.sendto(prepared_data, (ip, udp_to_send))
-        logging.info('Sending UDP package to another server...')
+        prepared_data = json.dumps(data).encode('utf-8')
+        try:
+            sock.sendto(prepared_data, (ip, udp_to_send))
+            logging.info('Sending UDP package to another server...')
+        except:
+            pass
 
-    def receive_data(self):
+    def receive_data(self, storage):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.bind((ip, udp_port))
         while True:
             data, address = sock.recvfrom(buffer_size)
+            data = json.loads(data.decode('utf-8'))
+            try:
+                if data['leader'] == True:
+                    self.partition_leader_exists = True
+                else:
+                    self.leader_data[data['port']] = data['leader']
+                    logging.info(f'{len(self.leader_data)} {self.leader_data} {len(ports_to_send)}')
+                    if len(self.leader_data) == len(ports_to_send):
+                        self.find_partition_leader()
+                    logging.info(f'Leader dict {self.leader_data}')
+            except KeyError:
+                storage.storage = deepcopy(data)
             logging.info(f'Receiving UDP package from another server...')
             logging.info(f'Received: {data}')
+
+    def find_partition_leader(self):
+        s = 0
+        for i in self.leader_data:
+            s += self.leader_data[i]
+        partition_leader = s % len(self.leader_data)
+        self.leader_data.clear()
+        if server_id == partition_leader:
+            self.is_partition_leader = True
+            logging.info(f'Partition leader address: http://' + ip + ':' + str(port) + '/')
+            threading.Thread(target=lambda: self.app.run(port=port, host="0.0.0.0", debug=True, use_reloader=False)).start()
+        self.partition_leader_exists = True
+        for destination_port in ports_to_send:
+            self.send_data({'port': udp_port, 'leader': True}, destination_port)
+
+    def set_partition_leader(self, app):
+        self.app = app
+        while True:
+            if not self.partition_leader_exists:
+                leader_data = {'port': udp_port, 'leader': self.leader_value}
+                for destination_port in ports_to_send:
+                    self.send_data(leader_data, destination_port)
+                sleep(5)
--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 import threading
 import logging
 import socket
+from time import sleep
 from flask import Flask, request, jsonify
 
 from config import *
 from Components_logic.server_communication import *
+from Components_logic.storage import *
 
 # initialize the logger mode
 logging.basicConfig(level=logging.DEBUG)
@@ -12,27 +14,33 @@ logging.basicConfig(level=logging.DEBUG)
 # initialize the server (app)
 app = Flask(__name__)
 server_communication = ServerCommunication()
+storage = Storage()
 
 
 @app.route('/', methods=['POST', 'GET', 'DELETE'])
 def receive_client_request():
     if request.method == 'POST':
-        data = request.json
+        received_data = request.json
         logging.info(f'Receiving new data to create or update')
-        return jsonify(data)
+        threading.Thread(target=storage.create_update_data, args=(received_data,)).start()
+        return jsonify(received_data)
     elif request.method == 'GET':
-        data = request.json
+        requested_key = request.json
         logging.info(f'Some data has been requested')
-        return 'Some data send'
+        requested_data = storage.get_data(requested_key['key'])
+        return requested_data
     elif request.method == 'DELETE':
-        data = request.json
+        data_to_delete = request.json
         logging.info(f'Some data requested to be deleted')
-        return 'Deleted'
+        threading.Thread(target=storage.delete_data, args=(data_to_delete['key'],)).start()
+        return 'Requested data has been deleted'
 
 
 if __name__ == "__main__":
-    # threading.Thread(target=lambda: app.run(port=8000, host="0.0.0.0", debug=True, use_reloader=False)).start()
-    threading.Thread(target=server_communication.receive_data).start()
-    while True:
-        data = input()
-        server_communication.send_data(data, 5000)
+    threading.Thread(target=server_communication.set_partition_leader, args=(app,)).start()
+    threading.Thread(target=server_communication.receive_data, args=(storage,)).start()
+    # threading.Thread(target=server_communication.set_partition_leader, args=(app,)).start()
+    # threading.Thread(target=lambda: app.run(port=port, host="0.0.0.0", debug=True, use_reloader=False)).start()
+    # while True:
+    #     data = input()
+    #     server_communication.send_data(data, 5000)
